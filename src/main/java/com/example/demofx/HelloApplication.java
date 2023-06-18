4608e948ff8d28809ee82b5b982f60ceff89dc54
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-06-18 20:10:23 +0300

    change starter of app

--- src/main/java/com/example/demofx/HelloApplication.java
+++ src/main/java/com/example/demofx/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.demofx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +13,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        BorderPane root = new BorderPane();
+        Label text = new Label("Hello, world!!");
+        text.setFont(new Font(32));
+        root.setCenter(text);
+        Scene scene = new Scene(root, 400, 150);
+
+        stage.setTitle("Title massage");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
